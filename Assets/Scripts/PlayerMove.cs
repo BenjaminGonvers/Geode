d2d1811e8ad878149a35dead4750d47cb37495d6
JonathanKlukachFernandez <jonathan.klukach.fernandez@gmail.com>
2022-03-10 16:50:29 +0100

    Level 1 Stars system +  Endlevel

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -43,4 +43,12 @@ public class PlayerMove : MonoBehaviour
 
         GetComponent<Rigidbody2D>().AddForce(impulseForce,ForceMode2D.Impulse);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Star"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
 }
