93f45ed7b089a69ba5616dd18e363647d79e93e9
BenjaminGonvers <gonversbenjamin@gmail.com>
2022-03-10 16:59:45 +0100

    Ajouts du test cameras et correction du roullement de la géode. Ajouts du la comunication playerInput au objet enfant.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
@@ -10,6 +11,7 @@ public class PlayerMove : MonoBehaviour
     [SerializeField]private float _maxForcePut;
 
     private Vector2 _posAim = new Vector2(0f, 0f);
+    private Vector2 _normalizedAim = new Vector2(0f, 0f);
 
     // Start is called before the first frame update
     void Start()
@@ -20,27 +22,29 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GetComponent<Rigidbody2D>().velocity.y <= 0.1f)
-        {
-            float newX = GetComponent<Rigidbody2D>().velocity.x - 1;
-            if (newX <= 1f)
-            {
-                newX = 0f;
-            }
-            GetComponent<Rigidbody2D>().velocity.Set(newX, 0f);
-        }
+       
     }
 
     public void InAim(InputAction.CallbackContext context)
     {
         _posAim = context.ReadValue<Vector2>();
+        if (_posAim.y != 0 || _posAim.x != 0)
+        {
+            _normalizedAim = _posAim.normalized;
+        }
+        else
+        {
+            _normalizedAim = new Vector2(0f, 0f);
+        }
     }
 
     public void LaunchGeode(InputAction.CallbackContext context)
     {
+        if (context.performed)
+        {
+            Vector2 impulseForce = _normalizedAim * _maxForcePut * -1f;
 
-        Vector2 impulseForce = _posAim * _maxForcePut * -1f;
-
-        GetComponent<Rigidbody2D>().AddForce(impulseForce,ForceMode2D.Impulse);
+            GetComponent<Rigidbody2D>().AddForce(impulseForce, ForceMode2D.Impulse);
+        }
     }
 }
