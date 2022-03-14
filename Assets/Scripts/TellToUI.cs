3aa909675266527e946b1dd54a1196df2c48e8dd
BenjaminGonvers <gonversbenjamin@gmail.com>
2022-03-14 14:53:16 +0100

    Adding player camera that react to player aim.

--- Assets/Scripts/TellToUI.cs
+++ Assets/Scripts/TellToUI.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class TellToUI : MonoBehaviour
 {
     [SerializeField] private ArrowAim _arrowAim;
+    [SerializeField] private playerCamera _playerCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -24,10 +25,13 @@ public class TellToUI : MonoBehaviour
 
     public void AimingUI(InputAction.CallbackContext context)
     {
-        Vector2 posAim = context.ReadValue<Vector2>().normalized;
-        
+        Vector2 posAimNormalized = context.ReadValue<Vector2>().normalized;
 
-        _arrowAim.transform.position = (this.transform.position + new Vector3(posAim.x *-1F, posAim.y*-1F, 0));
-        
+        Vector2 posAim = context.ReadValue<Vector2>();
+
+        _arrowAim.transform.position = (this.transform.position + new Vector3(posAimNormalized.x *-1F, posAimNormalized.y*-1F, 0));
+
+        _playerCamera.transform.position = (this.transform.position + new Vector3(posAim.x * -3f, posAim.y * -3f, -10f));
+
     }
 }
