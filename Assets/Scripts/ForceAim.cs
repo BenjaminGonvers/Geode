31d5c610050ef00041366c3828efcd0460a75381
BenjaminGonvers <gonversbenjamin@gmail.com>
2022-03-17 13:43:55 +0100

    Adding option for double jump and remplace the addforce to geode for jumping to setvelocity for the same utility.

--- Assets/Scripts/ForceAim.cs
+++ Assets/Scripts/ForceAim.cs
@@ -16,22 +16,25 @@ public class ForceAim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //see if we need to display(if no direction from the player, we don't display the charging bar)
         if (transform.localPosition.y == 0 && transform.localPosition.x == 0)
         {
             GetComponentInChildren<Renderer>().enabled = false;
-
             _chargingBar.GetComponent<Renderer>().enabled = false;
 
         }
         else
         {
+
             GetComponentInChildren<Renderer>().enabled = true;
             _chargingBar.GetComponent<Renderer>().enabled = true;
 
+            //calculate the Z axis for the rotational position and setting it
             float newAngle = 0;
             Vector3 relative = transform.InverseTransformPoint(_playerTransform.position);
             newAngle = Mathf.Atan2(relative.x, relative.y) * Mathf.Rad2Deg;
             transform.Rotate(0, 0, -newAngle + 90);
+
         }
 
     }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,6 +10,7 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private ChargingBar _chargingBar;
 
     [SerializeField]private float _maxForcePut;
+    [SerializeField] private float _maxForceDoubleJump;
 
 
     private Vector2 _posAim = new Vector2(0f, 0f);
@@ -17,6 +18,8 @@ public class PlayerMove : MonoBehaviour
 
     private bool _isCharging = false;
     private float _charge = 0;
+
+    private bool _alreadyDoubleJump = false;
 
 
     // Start is called before the first frame update
@@ -40,6 +43,7 @@ public class PlayerMove : MonoBehaviour
 
         _chargingBar.transform.localScale = new Vector3(_charge / 100, 1, 1);
 
+       
     }
 
     public void InAim(InputAction.CallbackContext context)
@@ -53,34 +57,51 @@ public class PlayerMove : MonoBehaviour
         {
             _normalizedAim = new Vector2(0f, 0f);
         }
+
+        
     }
 
     public void LaunchGeode(InputAction.CallbackContext context)
     {
-        
-
-        if (context.started)
+        if (GetComponent<Rigidbody2D>().velocity == Vector2.zero)
         {
-            _isCharging = true;
+
+
+         if (context.started)
+         {
+             _isCharging = true;
+         }
+
+         if (context.canceled && _isCharging)
+         {
+             _isCharging = false;
+
+             if (_charge < 20f)
+             {
+                    _charge = 20f;
+             }
+
+             Vector2 impulseForce = _normalizedAim * _maxForcePut * -1f * _charge / 100f;
+
+             setVelocityGeode(impulseForce);
+
+             _charge = 0f;
+
+             _alreadyDoubleJump = false;
+
+         }
+        }
+        else
+        {
+            //if (!_alreadyDoubleJump && context.started)
+            //{
+            //    Vector2 impulseForce = _normalizedAim * _maxForceDoubleJump * -1f;
+            //    setVelocityGeode(impulseForce);
+            //    _alreadyDoubleJump = true;
+            //}
         }
 
-        if (context.canceled)
-        {
-            _isCharging = false;
-
-            if (_charge < 20f)
-            {
-                _charge = 20f;
-            }
-
-            Vector2 impulseForce = _normalizedAim * _maxForcePut * -1f * _charge / 100f;
-
-            PropulseGeode(impulseForce);
-
-            _charge = 0f;
-        }
-
-    }
+}
 
     private void PropulseGeode(Vector2 impulseForce)
     {
@@ -88,6 +109,9 @@ public class PlayerMove : MonoBehaviour
         GetComponent<Rigidbody2D>().AddForce(impulseForce, ForceMode2D.Impulse);
     }
 
-
+    private void setVelocityGeode(Vector2 newVelocity)
+    {
+        GetComponent<Rigidbody2D>().velocity = newVelocity;
+    }
 
 }
