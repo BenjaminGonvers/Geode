709ecc36e5586bfd39b6344b2a11ef4f9f699920
JonathanKlukachFernandez <jonathan.klukach.fernandez@gmail.com>
2022-03-22 17:43:35 +0100

    Player death + copie prefab player

---
+++ Assets/Scripts/Weed.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Weed : MonoBehaviour
+{
+    public int Respawn;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(Respawn);
+        }
+    }
+
+}
