ff847bc4a597ba25c003ce2e86ce5e927ccb4a57
JonathanKlukachFernandez <jonathan.klukach.fernandez@gmail.com>
2022-04-08 18:01:20 +0200

    UIStars + Finishline change level

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public int level;
+    public float[] position;
+
+    public PlayerData(Player player)
+    {
+        level = player.level;
+
+    }
+}
