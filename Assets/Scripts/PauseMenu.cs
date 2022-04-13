6a947bfc1d8c13fd45bee81058bd0855b4f1c6de
JonathanKlukachFernandez <jonathan.klukach.fernandez@gmail.com>
2022-04-13 14:20:13 +0200

    Menus with controller + Arrows appearing correctly

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,6 +4,7 @@ using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class PauseMenu : MonoBehaviour
 {
